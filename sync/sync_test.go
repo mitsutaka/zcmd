c9471f2e39f9608ac5e9a7b163210dd4c89d4ab1
Mitz Amano <mitz@linux.com>
2019-01-26 12:23:27 +0900

    Add sync_test.go

---
+++ sync/sync_test.go
@@ -0,0 +1,107 @@
+package sync
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/mitsutaka/zcmd"
+)
+
+func TestBackup(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		cfgs     []zcmd.SyncInfo
+		args     []string
+		expected []zcmd.SyncInfo
+	}{
+		{
+			cfgs: []zcmd.SyncInfo{
+				{
+					Name:        "foo",
+					Source:      "/foo",
+					Destination: "/tmp/foo",
+				},
+				{
+					Name:        "bar",
+					Source:      "/bar",
+					Destination: "/tmp/bar",
+					Excludes:    []string{"aaa", "bbb"},
+				},
+			},
+			args: []string{},
+			expected: []zcmd.SyncInfo{
+				{
+					Name:        "foo",
+					Source:      "/foo",
+					Destination: "/tmp/foo",
+				},
+				{
+					Name:        "bar",
+					Source:      "/bar",
+					Destination: "/tmp/bar",
+					Excludes:    []string{"aaa", "bbb"},
+				},
+			},
+		},
+		{
+			cfgs: []zcmd.SyncInfo{
+				{
+					Name:        "foo",
+					Source:      "/foo",
+					Destination: "/tmp/foo",
+				},
+				{
+					Name:        "bar",
+					Source:      "/bar",
+					Destination: "/tmp/bar",
+					Excludes:    []string{"aaa", "bbb"},
+				},
+			},
+			args: []string{"foo"},
+			expected: []zcmd.SyncInfo{
+				{
+					Name:        "foo",
+					Source:      "/foo",
+					Destination: "/tmp/foo",
+				},
+			},
+		},
+		{
+			cfgs: []zcmd.SyncInfo{
+				{
+					Name:        "foo",
+					Source:      "/foo",
+					Destination: "/tmp/foo",
+				},
+				{
+					Name:        "bar",
+					Source:      "/bar",
+					Destination: "/tmp/bar",
+					Excludes:    []string{"aaa", "bbb"},
+				},
+			},
+			args: []string{"foo", "bar"},
+			expected: []zcmd.SyncInfo{
+				{
+					Name:        "foo",
+					Source:      "/foo",
+					Destination: "/tmp/foo",
+				},
+				{
+					Name:        "bar",
+					Source:      "/bar",
+					Destination: "/tmp/bar",
+					Excludes:    []string{"aaa", "bbb"},
+				},
+			},
+		},
+	}
+
+	for _, c := range cases {
+		syncInfo := findTargetSyncs(c.cfgs, c.args)
+		if !reflect.DeepEqual(syncInfo, c.expected) {
+			t.Errorf("%#v != %#v", syncInfo, c.expected)
+		}
+	}
+}
