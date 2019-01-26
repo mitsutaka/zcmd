419c06976dc46c3d98d9b7ca47bde4aa397238a7
Mitz Amano <mitz@linux.com>
2019-01-26 12:44:48 +0900

    Fix backup test

--- backup_test.go
+++ backup_test.go
@@ -1,12 +1,18 @@
 package zcmd
 
 import (
+	"os"
 	"reflect"
 	"testing"
 )
 
 func TestBackup(t *testing.T) {
 	t.Parallel()
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	cases := []struct {
 		cfg      BackupConfig
@@ -20,9 +26,9 @@ func TestBackup(t *testing.T) {
 			},
 			dryRun: true,
 			expected: map[string][]string{
-				"/":     {"/usr/bin/sudo", "-E", "/usr/bin/rsync", "-avxRP", "--stats", "--delete", "--dry-run", "", "/", "/backup/nuc/backup-0000-00-00-000000"},
-				"/boot": {"/usr/bin/sudo", "-E", "/usr/bin/rsync", "-avxRP", "--stats", "--delete", "--dry-run", "", "/boot", "/backup/nuc/backup-0000-00-00-000000"},
-				"/home": {"/usr/bin/sudo", "-E", "/usr/bin/rsync", "-avxRP", "--stats", "--delete", "--dry-run", "", "/home", "/backup/nuc/backup-0000-00-00-000000"},
+				"/":     {"/usr/bin/sudo", "-E", "/usr/bin/rsync", "-avxRP", "--stats", "--delete", "--dry-run", "", "/", "/backup/" + hostname + "/backup-0000-00-00-000000"},
+				"/boot": {"/usr/bin/sudo", "-E", "/usr/bin/rsync", "-avxRP", "--stats", "--delete", "--dry-run", "", "/boot", "/backup/" + hostname + "/backup-0000-00-00-000000"},
+				"/home": {"/usr/bin/sudo", "-E", "/usr/bin/rsync", "-avxRP", "--stats", "--delete", "--dry-run", "", "/home", "/backup/" + hostname + "/backup-0000-00-00-000000"},
 			},
 		},
 		{
@@ -32,9 +38,9 @@ func TestBackup(t *testing.T) {
 			},
 			dryRun: false,
 			expected: map[string][]string{
-				"/":     {"/usr/bin/sudo", "-E", "/usr/bin/rsync", "-avxRP", "--stats", "--delete", "", "/", "/backup/nuc/backup-0000-00-00-000000"},
-				"/boot": {"/usr/bin/sudo", "-E", "/usr/bin/rsync", "-avxRP", "--stats", "--delete", "", "/boot", "/backup/nuc/backup-0000-00-00-000000"},
-				"/home": {"/usr/bin/sudo", "-E", "/usr/bin/rsync", "-avxRP", "--stats", "--delete", "", "/home", "/backup/nuc/backup-0000-00-00-000000"},
+				"/":     {"/usr/bin/sudo", "-E", "/usr/bin/rsync", "-avxRP", "--stats", "--delete", "", "/", "/backup/" + hostname + "/backup-0000-00-00-000000"},
+				"/boot": {"/usr/bin/sudo", "-E", "/usr/bin/rsync", "-avxRP", "--stats", "--delete", "", "/boot", "/backup/" + hostname + "/backup-0000-00-00-000000"},
+				"/home": {"/usr/bin/sudo", "-E", "/usr/bin/rsync", "-avxRP", "--stats", "--delete", "", "/home", "/backup/" + hostname + "/backup-0000-00-00-000000"},
 			},
 		},
 	}
