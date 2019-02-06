df70f134f6ccb0887a3001ba22db3cf524558f6d
Mitz Amano <mitz@linux.com>
2019-02-07 07:01:05 +0900

    Fix backup command to not show usage if return error

--- pkg/z/cmd/backup.go
+++ pkg/z/cmd/backup.go
@@ -2,9 +2,11 @@ package cmd
 
 import (
 	"context"
+	"os"
 
 	"github.com/cybozu-go/well"
 	"github.com/mitsutaka/zcmd"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -19,14 +21,19 @@ var backupCmd = &cobra.Command{
 	Use:   "backup",
 	Short: "backup local data to the remote server",
 	Long:  `backup starts backup process local data to the remote server`,
-	RunE: func(cmd *cobra.Command, args []string) error {
+	Run: func(cmd *cobra.Command, args []string) {
 		bk := zcmd.NewBackup(&cfg.Backup, backupOpts.dryRun)
 
 		well.Go(func(ctx context.Context) error {
 			return bk.Do(ctx)
 		})
 		well.Stop()
-		return well.Wait()
+		err := well.Wait()
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+		os.Exit(0)
 	},
 }
 
