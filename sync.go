86f1f9ac099167c0ed4d6145ad6ebaaa8a448c37
Mitz Amano <mitz@linux.com>
2020-02-19 18:35:53 +0800

    Remove -z flag

--- sync.go
+++ sync.go
@@ -38,7 +38,7 @@ func (s *Sync) Do(ctx context.Context) error {
 
 // GenerateCmd generates rsync command
 func (s *Sync) generateCmd() ([]rsyncClient, error) {
-	var optsRsync = []string{"-avzP", "--stats", "--delete", "--delete-excluded"}
+	var optsRsync = []string{"-avP", "--stats", "--delete", "--delete-excluded"}
 
 	targetSyncs := findTargetSyncs(s.cfgSyncs, s.argSyncs)
 
